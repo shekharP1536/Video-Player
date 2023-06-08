49dcb1733948eccede9cf1d92b3c3e1be54409a9
shekharP1536 <chandrashekharpachlore@gmail.com>
2023-06-08 11:17:20 +0530

    fixed known issue

--- de.js
+++ de.js
@@ -14,6 +14,9 @@ const fullscreenBtn = document.getElementById('fullscreen-btn');
 const settingsBtn = document.getElementById('settings-btn');
 const slider = document.getElementById("VolumeRange");
 const settings = document.getElementById("settings-btn");
+const seeker = document.getElementById("seeker");
+const videoContainer = document.getElementById("videoContainer");
+videoContainer.addEventListener("click", togglePlayPause);
 playPauseBtn.addEventListener('click', togglePlayPause);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener("load", updateProgress)
@@ -27,15 +30,32 @@ fullscreenBtn.addEventListener('click', toggleFullscreen);
 slider.oninput = function() {
     video.volume = this.value / 100;
   }
+// Get the progress bar element
 
-// Tooltip handling
-const tooltips = document.querySelectorAll('[data-tooltip]');
-tooltips.forEach((tooltip) => {
-  tooltip.addEventListener('mouseenter', showTooltip);
-  tooltip.addEventListener('mouseleave', hideTooltip);
+
+// Get the tooltip element
+var tooltip = document.querySelector(".tooltipsForTimestamp");
+
+// Add a mousemove event listener to the progress bar
+progressBarBar.addEventListener("mousemove", function(e) {
+  // Get the mouse x coordinate relative to the progress bar
+  var mouseX = e.offsetX;
+
+  // Get the width of the progress bar
+  var progressBarWidth = progressBarBar.offsetWidth;
+
+  // Calculate the percentage of the mouse position
+  var percentage = mouseX / progressBarWidth;
+  const seekTime = (event.offsetX / progressBarBar.offsetWidth) * video.duration;
+  const seekTimeFormatted = formatTime(seekTime);
+  tooltip.innerHTML = seekTimeFormatted;
+
+  // Update the tooltip left position
+  tooltip.style.left = percentage * 98 + "%";
 });
 
-const    rangeInput = document.querySelector('.range input');
+
+const rangeInput = document.querySelector('.range input');
 const rangeSpan = document.querySelector('.range span');
 
 rangeInput.addEventListener('mousemove', function() {
@@ -47,33 +67,19 @@ let isSeeking = false;
 function setting(){
   // settings.style.rotate = "50"
 }
-function showTooltip() {
-  const tooltip = this.dataset.tooltip;
-  const tooltipElement = document.createElement('div');
-  tooltipElement.classList.add('tooltip');
-  tooltipElement.textContent = tooltip;
-  this.appendChild(tooltipElement);
-  setTimeout(() => {
-    tooltipElement.style.opacity = '1';
-  }, 10);
-}
-
-function hideTooltip() {
-  const tooltipElement = this.querySelector('.tooltip');
-  tooltipElement.style.opacity = '0';
-  setTimeout(() => {
-    this.removeChild(tooltipElement);
-  }, 300);
-}
-
 function togglePlayPause() {
+  var animationspan = document.getElementById("videoanimation")
   if (video.paused || video.ended) {
     video.play();
     playPauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    animationspan.innerHTML = '<i class="fas fa-pause"></i>';
+    animationspan.classList.add("animate");
     playPauseBtn.dataset.tooltip = 'pause';
   } else {
     video.pause();
     playPauseBtn.innerHTML = '<i class="fas fa-play"></i>';
+    animationspan.innerHTML = '<i class="fas fa-play"></i>';
+    animationspan.classList.remove("animate");
     playPauseBtn.dataset.tooltip = 'play';
   }
 }
@@ -83,8 +89,8 @@ function updateProgress() {
   const totalTime = formatTime(video.duration);
   timeText.textContent = `${currentTime} / ${totalTime}`;
   const progress = (video.currentTime / video.duration) * 100;
-  console.log(totalTime);
   progressBar.style.width = `${progress}%`;
+  seeker.style.left = `${progress}%`;
 }
 
 function seek(event) {
@@ -115,32 +121,78 @@ function toggleCaption() {
     captionBtn.textContent = (textTrack.mode === 'showing') ? 'Caption: On' : 'Caption: Off';
   }
 }
+
 function toggleTheaterMode() {
-  const videoPlayer = document.getElementById('video-player');
   const theaterControls = document.getElementById('theater-controls');
 
   if (!videoPlayer.classList.contains('theater-mode')) {
-    video.classList.add('theater-mode');
-    videoPlayer.classList.add('theater-mode-controls');
-    theaterBtn.dataset.tooltip = 'Default View';
+    videoPlayer.classList.add('theater-mode');
+    theaterBtn.dataset.tooltip = 'Exit Theater Mode';
+
+    // Create theater controls if they don't exist
+    if (!theaterControls) {
+      const controls = document.getElementById('controls');
+      theaterControls = document.createElement('div');
+      theaterControls.id = 'theater-controls';
+      theaterControls.innerHTML = `
+        <button id="theater-play-pause-btn" class="control-btn" data-tooltip="Play">
+          <i class="fas fa-play"></i>
+        </button>
+        <div id="theater-time" class="control-text">0:00 / 0:00</div>
+        <div id="theater-progress-bar" class="control-bar" data-tooltip="">
+          <div id="theater-progress"></div>
+        </div>
+      `;
+      controls.appendChild(theaterControls);
+
+      const theaterPlayPauseBtn = document.getElementById('theater-play-pause-btn');
+      const theaterTimeText = document.getElementById('theater-time');
+      const theaterProgressBar = document.getElementById('theater-progress-bar');
+      const theaterProgressBarBar = document.getElementById('theater-progress');
+
+      theaterPlayPauseBtn.addEventListener('click', togglePlayPause);
+      video.addEventListener('timeupdate', () => {
+        theaterTimeText.textContent = `${formatTime(video.currentTime)} / ${formatTime(video.duration)}`;
+        const progress = (video.currentTime / video.duration) * 100;
+        theaterProgressBar.style.width = `${progress}%`;
+      });
+      theaterProgressBarBar.addEventListener('click', seek);
+}
   } else {
     videoPlayer.classList.remove('theater-mode');
-    videoPlayer.classList.remove('theater-mode-controls');
     theaterBtn.dataset.tooltip = 'Theater Mode';
+    if (theaterControls) {
+      theaterControls.remove();
+    }
   }
 }
 
 function toggleFullscreen() {
-  if (video.requestFullscreen) {
-    videoPlayer.requestFullscreen();
+  if (videoPlayer.requestFullscreen) {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      videoPlayer.requestFullscreen();
+    }
   } else if (video.mozRequestFullScreen) {
-    videoPlayer.mozRequestFullScreen();
+    if (document.mozFullScreenElement) {
+      document.mozCancelFullScreen();
+    } else {
+      videoPlayer.mozRequestFullScreen();
+    }
   } else if (video.webkitRequestFullscreen) {
-    videoPlayer.webkitRequestFullscreen();
+    if (document.webkitFullscreenElement) {
+      document.webkitExitFullscreen();
+    } else {
+      videoPlayer.webkitRequestFullscreen();
+    }
   } else if (video.msRequestFullscreen) {
-    videoPlayer.msRequestFullscreen();
+    if (document.msFullscreenElement) {
+      document.msExitFullscreen();
+    } else {
+      videoPlayer.msRequestFullscreen();
+    }
   }
-  videoPlayer.style.backgroundColor = "white";
 }
 
 function formatTime(time) {
